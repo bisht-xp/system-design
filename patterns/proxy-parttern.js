b769b8ba1b15527dd4dcf60bb897160b280c39af
bisht-xp <bishtkamal2907@gmail.com>
2026-02-18 12:47:35 +0530

    add: added proxy pattern

---
+++ patterns/proxy-parttern.js
@@ -0,0 +1,27 @@
+const person = {
+  name: "Shaktimaan",
+  age: 23,
+  gender: "Male",
+};
+
+const proxiedPerson = new Proxy(person, {
+  get(obj, prop) {
+    if (prop === "gender") {
+      console.log("Gender is a write only property be exposed!");
+    } else {
+      return obj[prop];
+    }
+  },
+
+  set(obj, prop, value) {
+    if (prop === "age") {
+      if (value < 18 || value > 50) {
+        console.log("Age value should be between 18 and 50");
+      } else {
+        obj[prop] = value;
+      }
+    } else {
+      obj[prop] = value;
+    }
+  },
+});
